4bd1818e0479fb515365b005cccffb9a881d2362
WilmerDamas10x10 <wilmerepara7@hotmail.com>
2025-12-11 00:26:01 -0500

    Fix: sincronizar exports de piezas

--- src/rules/pieces/index.js
+++ src/rules/pieces/index.js
@@ -1,19 +1,24 @@
 // src/rules/pieces/index.js
-import * as pawn from "./pawn.js";
-import * as queen from "./queen.js";
+// Re-export centralizado de piezas para el motor.
 
-// Peón
-export const genPawnMoves     =
-  pawn.genPawnMoves     || pawn.generatePawnMoves     || pawn.movimientosPeon;
-export const genPawnCaptures  =
-  pawn.genPawnCaptures  || pawn.generatePawnCaptures  || pawn.capturasPeon;
-export const pawnValue        =
-  (typeof pawn.pawnValue === "number" ? pawn.pawnValue : 1);
+// PEONES
+export {
+  generatePawnMoves,
+  generatePawnCaptures,
+  genPawnMoves,
+  genPawnCaptures,
+  movimientosPeon,
+  capturasPeon,
+  pawnValue,
+} from "./pawn.js";
 
-// Dama
-export const genQueenMoves    =
-  queen.genQueenMoves   || queen.generateQueenMoves   || queen.movimientosDama;
-export const genQueenCaptures =
-  queen.genQueenCaptures|| queen.generateQueenCaptures|| queen.capturasDama;
-export const queenValue       =
-  (typeof queen.queenValue === "number" ? queen.queenValue : 1.5);
+// DAMAS
+export {
+  genQueenMoves,
+  genQueenCaptures,
+  generateQueenMoves,
+  generateQueenCaptures,
+  movimientosDama,
+  capturasDama,
+  queenValue,
+} from "./queen.js";
